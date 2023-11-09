c4ce4c93ecd5d304fc710b821af57aa244512570
mdelgadonyc <moises.f.delgado@gmail.com>
2023-11-09 18:33:19 -0500

    url onfig, map / to home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -4,12 +4,9 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_corrects_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
 
-
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertTrue(response, "<html>")
+        self.assertTrue(response, "</html>")
